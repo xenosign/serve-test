5de1dd3ac226f8d4542ae1d981efd0dde0a296b8
kdtTetz <xenosign@naver.com>
2023-05-09 01:58:36 +0900

    express 배포 코드 추가

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+// const path = require("path");
+
+const app = express();
+
+app.set("port", process.env.PORT || 3000);
+
+// app.use(express.static(path.join(__dirname, "/build")));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname, "/index.html");
+});
+
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 포트에서 대기중..");
+});
