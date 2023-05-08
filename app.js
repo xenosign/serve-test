b6cb5e7f516c1b958075f1a19cd631dfc5b3900b
kdtTetz <xenosign@naver.com>
2023-05-09 02:03:21 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const path = require("path");
+const path = require("path");
 
 const app = express();
 
@@ -8,7 +8,7 @@ app.set("port", process.env.PORT || 3000);
 // app.use(express.static(path.join(__dirname, "/build")));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname, "/index.html");
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
 
 app.listen(app.get("port"), () => {
